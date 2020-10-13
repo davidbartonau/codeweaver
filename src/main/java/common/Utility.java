b83668c6bd8ee5a5612f099447baeec6785fb899
rajan_1905@live.com <rajan_1905@live.com>
2020-10-13 16:30:09 +0100

    Sha calculation of PreOpen-Open & Open to PostOpen * Open to Close

--- src/main/java/common/Utility.java
+++ src/main/java/common/Utility.java
@@ -7,9 +7,26 @@ import java.util.List;
 
 public class Utility {
 
-    public static String calculateSha1OfRows(List<String> rows){
+    public static String calculateSha1OfRows(String rows){
         return Hashing.sha1().
-                hashString( String.join(System.lineSeparator(), rows), Charsets.UTF_8)
+                hashString( rows, Charsets.UTF_8)
                 .toString();
+    }
+
+    public static String insertSha(String row, String sha){
+        StringBuilder builder = new StringBuilder();
+        int count = 0;
+        for(int i=0;i<row.length();i++){
+            if(row.charAt(i) == '}') {
+                if(count == 1){
+                    builder.append(row.substring(0, i));
+                    builder.append(", sha: " + sha);
+                    builder.append(row.substring(i));
+                    break;
+                }
+                else count++;
+            }
+        }
+        return builder.toString();
     }
 }
--- src/main/java/reader/CodeReader.java
+++ src/main/java/reader/CodeReader.java
@@ -1,6 +1,7 @@
 package reader;
 
 import common.Tags;
+import common.Utility;
 
 import java.io.File;
 import java.util.*;
@@ -33,7 +34,7 @@ public class CodeReader {
         Map<Integer, String> map = new LinkedHashMap<>();
         List<Tags> tags = Arrays.asList(Tags.values());
 
-        for(int i=0; i<rows.size(); i++){
+        /*for(int i=0; i<rows.size(); i++){
             String row = rows.get(i);
             if(row.trim().isEmpty()) continue;
             for(Tags tag: tags){
@@ -42,8 +43,76 @@ public class CodeReader {
                     break;
                 }
             }
+        }*/
+
+        for(int i=0;i<rows.size();i++){
+            String row = rows.get(i);
+
+            if(row.contains(Tags.PREOPEN.label)) {
+                processForPreOpenToOpen(rows, i);
+            }
+            if(row.contains(Tags.OPEN.label)) {
+                processForOpenToPostOpen(rows, i);
+            }
         }
 
         return map;
+    }
+
+    private void processForPreOpenToOpen(List<String> list, int i){
+        String row = list.get(i);
+        String id = (row.split("}")[0]).split(":")[1];
+        int j=0;
+        for(j=i;j<= list.size();j++){
+            if(list.get(j).contains(Tags.OPEN.label+":"+id)) break;
+            else continue;
+        }
+        String sha = null;
+        if (i == j) {
+            // Found on same line
+            String s = (row.split("\\*/")[1]).split("/*")[0];
+             sha = Utility.calculateSha1OfRows(s);
+        }
+        else{
+            StringBuilder builder = new StringBuilder();
+            builder.append(row.lastIndexOf("/") != row.length()-1 ? list.get(i).split("\\*/")[1] : "");
+            for(int x = i+1; x<j;x++){
+                builder.append(list.get(x));
+            }
+            builder.append(list.get(j).split("/*")[0]);
+            sha = Utility.calculateSha1OfRows(builder.toString());
+        }
+        list.remove(i);
+        list.add(i, Utility.insertSha(row, sha));
+    }
+
+    private void processForOpenToPostOpen(List<String> list, int i){
+        String row = list.get(i);
+        String id = (row.split("}")[0]).split(":")[1];
+        int j=0;
+        for(j=i;j<= list.size();j++){
+            if(list.get(j).contains(Tags.CLOSE.label+":"+id)) {
+                //Close found for the tag with same id
+            }
+            if(list.get(j).contains(Tags.POSTOPEN.label+":"+id)) break;
+            else continue;
+        }
+        String sha = null;
+        if (i == j) {
+            // Found on same line
+            String s = (row.split("\\*/")[1]).split("/*")[0];
+            sha = Utility.calculateSha1OfRows(s);
+        }
+        else{
+            StringBuilder builder = new StringBuilder();
+            builder.append(row.lastIndexOf("/") != row.length()-1 ? list.get(i).split("\\*/")[1] : "");
+            for(int x = i+1; x<j;x++){
+                builder.append(list.get(x));
+            }
+            builder.append(list.get(j).split("/*")[0]);
+            sha = Utility.calculateSha1OfRows(builder.toString());
+        }
+        list.remove(i);
+        list.add(i, Utility.insertSha(row, sha));
     }
 }
